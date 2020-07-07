39d6719404d2b6788776a7f5e353359451bf8a80
username <email>
2020-07-08 01:58:38 +0800

    cloud-provider-payment8001 init

---
+++ cloud-provider-payment8001/src/main/java/com/toolmanclub/springcloud/PaymentMain.java
@@ -0,0 +1,19 @@
+package com.toolmanclub.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Yujie Wang
+ * @date 2020/7/8 1:56
+ */
+@SpringBootApplication
+public class PaymentMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
